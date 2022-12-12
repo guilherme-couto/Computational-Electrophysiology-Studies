bae00a20321d9a88f73f7f63f4c6056bee1388d3
GuilhermeC <guiguimcouto@gmail.com>
2022-12-12 14:05:27 -0300

    Alterações sem avanço

--- omp-cable-eq-fhn.c
+++ omp-cable-eq-fhn.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
     // Infos
     float k, A, alpha, L, epsilon, gamma;
-    float d_x, d_t;
+    float d_x, d_t, d_t_exp;
 
     k = 2.0;
     A = 1.0;
@@ -40,18 +40,20 @@ int main(int argc, char *argv[])
 
     d_x = 1.0;
     d_t = 0.1;
+    d_t_exp = d_t / 1.0;
 
-    int T, M, N;
+    int T, M, M_exp, N;
 
     T = 80000;
 
-    M = T / d_t; // number of points in time
-    N = L / d_x; // number of points in space
+    M = T / d_t;            // number of points in time
+    M_exp = d_t / d_t_exp;  // number of points in time for explicit
+    N = L / d_x;            // number of points in space
 
     printf("Number of points in time = %d\n", M);
     printf("Number of points in space = %d\n", N);
 
-    // Matrix for v and w
+    // Array for v and w
     double *V;
     double *W;
     V = malloc(M * N * sizeof(double));
@@ -87,14 +89,58 @@ int main(int argc, char *argv[])
         
         for (i = 1; i < N - 1; i++)
         {
-            V[(n + 1) * N + i] = V[n * N + i] + d_t * ((k * (V[n * N + (i + 1)] - 2 * V[n * N + i] + V[n * N + (i - 1)]) / d_x / d_x) + (A * V[n * N + i] * (1 - V[n * N + i]) * (V[n * N + i] - alpha)) - W[n * N + i]);
+            V[(n + 1) * N + i] = V[n * N + i] + d_t * ((k * (V[n * N + (i + 1)] - 2 * V[n * N + i] + V[n * N + (i - 1)]) / d_x / d_x)); // + (A * V[n * N + i] * (1 - V[n * N + i]) * (V[n * N + i] - alpha)) - W[n * N + i]);
+            
+            V[(n + 1) * N + i] = V[(n + 1) * N + i] + d_t * ((A * V[n * N + i] * (1 - V[n * N + i]) * (V[n * N + i] - alpha)) - W[n * N + i]);
             W[(n + 1) * N + i] = W[n * N + i] + d_t * epsilon * (V[(n + 1) * N + i] - gamma * W[n * N + i]);
         }
 
-        // Condition
+        // Boundary Condition
         V[(n + 1) * N + 0] = V[(n + 1) * N + 1];
         V[(n + 1) * N + (N - 1)] = V[(n + 1) * N + (N - 2)];
     }
+
+    // Operator Splitting Method
+
+    /* double *v_reaction;
+    double *w_reaction;
+    v_reaction = malloc(M_exp * N * sizeof(double));
+    w_reaction = malloc(M_exp * N * sizeof(double));
+
+    for (int c = 0; c < N; c++)
+    {
+        v_reaction[c] = V[c];
+        w_reaction[c] = W[c];
+    }
+
+
+    for (int n = 0; n < M - 1; n++)
+    {
+        // Reaction
+        // Explicit Euler        
+
+        for (int c = 0; c < N; c++)
+        {
+            v_reaction[n * M + c] = V[n * M + c];
+            w_reaction[n * M + c] = W[n * M + c];
+        }
+
+        int i, j;
+
+        for (j = 0; j < M_exp; j++)
+        {
+            for (i = 1; i < N - 1; i++)
+            {
+                v_reaction[(j + 1) * N + i] = v_reaction[j * N + i] + d_t_exp * ((A * v_reaction[j * N + i] * (1 - v_reaction[j * N + i]) * (v_reaction[j * N + i] - alpha)) - w_reaction[j * N + i]);
+                w_reaction[(j + 1) * N + i] = w_reaction[j * N + i] + d_t_exp * epsilon * (v_reaction[(j + 1) * N + i] - gamma * w_reaction[j * N + i]);
+            }
+        }
+
+        // Diffusion
+        // Implicit Euler  
+
+    }   */  
+
 
     finish = omp_get_wtime();
     elapsed = finish - start;
@@ -120,6 +166,8 @@ int main(int argc, char *argv[])
 
     free(V);
     free(W);
+    /* free(v_reaction);
+    free(w_reaction); */
 
     return 0;
 }
